ef90d15ad9224b1ef88e4a8d378f2c5ad19939fe
unknown <sudip2tenber@gmail.com>
2023-12-13 13:41:08 +0600

    five

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $(function(){
     var mixer = mixitup('.mixit_main');
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+    var countDownDate = new Date("Jan 5, 2026 15:37:25").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
